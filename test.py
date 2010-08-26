0d1d0d100b63e8c7a6812e76633957a93d85355f
Marc Zinnschlag <marc@zpages.de>
2010-08-26 21:08:31 +0200

    updated test application

--- test.py
+++ test.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 
 import highgtk.app.staged
+import highgtk.entity
+import highgtk.data
+
+class TestStage (highgtk.app.staged.Stage):
+
+    def __init__ (self, app):
+        highgtk.app.staged.Stage.__init__ (self, app, "Stage 1")
+        data = (
+            highgtk.data.Text ("id1", "Some text"),
+            highgtk.data.Text ("id2", "Some other text"))
+        self.inquiry = highgtk.entity.Inquiry (data)
+        self.inquiry.title = "Some Input, please!"
+        self.inquiry.ok_method = "_quit_ok"
+        self.inquiry.cancel_method = "_quit_cancel"
+
+    def _quit_ok (self, inquiry, results):
+        print results
+        self.application.advance()
+
+    def _quit_cancel (self, inquiry):
+        self.application.advance()
+
+    def run (self):
+        self.add (self.inquiry)
 
 if __name__=="__main__":
     app = highgtk.app.staged.Application ("Test")
+    app.stages.append (TestStage (app))
     app.run()
