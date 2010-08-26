3e4f4438b4c5f722e6d16acf52bb24ee219ec8ce
Marc Zinnschlag <marc@zpages.de>
2010-08-26 13:07:59 +0200

    basic entity structure and application test

---
+++ highgtk/entity.py
@@ -0,0 +1,52 @@
+"""This module provides the entity tree."""
+
+import gtk
+
+class Entity:
+    """This is the entity base class."""
+
+    def __init__ (self):
+        self.children = []
+        self.parent = None
+
+    def add (self, entity):
+        """Make entity a child of self."""
+        assert entity.parent is None
+        entity.parent = self
+        self.children.append (entity)
+
+    def remove (self, remove_parent = True):
+        """Remove self from the entity graph."""
+        if self.parent and remove_parent:
+            self.parent.children.remove (self)
+        self.parent = None
+        for c in self.children:
+            c.remove (False)
+        self.children = []
+
+class Application (Entity):
+    """Entity that represents the whole application.
+
+    Note: The root entity must be an application (either an instance of this class or
+    a derived class.
+
+    """
+
+    def __init__ (self, name):
+        """
+        name: Applications name as presented to the user
+        """
+        Entity.__init__ (self)
+        self.name = name
+
+    def run (self):
+        """Run main loop (blocks until application is qutting)."""
+        gtk.main()
+
+def get_root (entity):
+    """Return the root for the given entity."""
+    assert entity is not None
+    while entity.parent is not None:
+        entity = entity.parent
+    assert instanceof (entity, Application)
+    return entity
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import highgtk.entity
+
+if __name__=="__main__":
+    app = highgtk.entity.Application ("Test")
+    app.run()
