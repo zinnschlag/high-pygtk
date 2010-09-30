e22af90d43aa4e2e6e95d5380f8aa97cc7007e1f
Marc Zinnschlag <marc@zpages.de>
2010-09-30 12:08:34 +0200

    put treeview into a scrolled window

--- highgtk/present/default/element.py
+++ highgtk/present/default/element.py
@@ -16,13 +16,15 @@ def remove_text (element):
 def add_table (element):
     widget = getattr (element, "present_widget", None)
     if widget is None:
-        element.present_widget = gtk.TreeView (element.document.data)
+        element.present_widget = gtk.ScrolledWindow()
+        view = gtk.TreeView (element.document.data)
+        element.present_widget.add_with_viewport (view)
         element.present_columns = []
         index = 0
         for c in element.document.columns:
             if c.label is not None:
                 column = gtk.TreeViewColumn (c.label)
-                element.present_widget.append_column (column)
+                view.append_column (column)
                 element.present_columns.append (column)
                 renderer = highgtk.cell.get_cell_renderer (c)
                 column.pack_start (renderer, True)
@@ -30,7 +32,8 @@ def add_table (element):
                 index = index + 1
         reorder = getattr (element.document, "reorder", None)
         if reorder is not None:
-            element.present_widget.set_reorderable (True)
+            view.set_reorderable (True)
+        view.show()
 
 def remove_table (element):
     widget = getattr (element, "present_widget", None)
